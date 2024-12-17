034cc636d3e4f27f4f817e7e3ad895e7d980ebe0
notpeddy <panisa.krt@gmail.com>
2024-12-17 10:01:08 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int column, int row)
+{
+
+if(column<=0 || row<=0){
+
+cout << "Invalid input";
+
+}else{
+
+for(int c=0; c<column; c++){
+
+for(int r=0; r<row; r++){
+
+cout<<"O";
+
+}
+
+cout << "\n";
+
+} }
+
+}
 
 int main(){
 	
